3dd007fa3de933b68a1bed158196edba3b891237
Gaston Briones <gbriones@cmd.com.ar>
2016-04-05 01:08:58 -0300

    add other fixed

--- index.php
+++ index.php
@@ -1,7 +1,64 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: gaston
- * Date: 23/03/16
- * Time: 17:24
- */
+<!DOCTYPE html>
+<html lang="es-ES">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width"/>
+    <meta name="description"
+          content="Iñaki es un restaurante de cocina vasca ubicado en la calle Moreno 1341, en el barrio Monserrat, Ciudad de Buenos Aires."/>
+    <!-- Links -->
+    <link href="css/normalize.css" rel="stylesheet" type="text/css">
+    <link href="css/gridlex.min.css" rel="stylesheet" type="text/css">
+    <link href="css/hover-min.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link href="css/mediaqueries.css" rel="stylesheet" type="text/css">
+    <link rel="shortcut icon" href="favicon.ico"/>
+    <title>Brabefil - Distribuidores de GVS - Life Sciences </title>
+</head>
+<body>
+<div class="site">
+    <section class="home">
+        <div class="wrapper content">
+            <div class="grid">
+                <div class="col-6 logo" push-left="off-3">
+                    <a href="#">
+                        <img src="images/logo.png"/>
+                    </a>
+                    <span class="slogan">
+                        DISTRIBUIDORES DE GVS - LIFE SCIENCES
+                    </span>
+                    <div class="gsv">
+                        <img src="images/logo_GVS.png" />
+                    </div>
+                </div>
+                <div class="col-2" push-left="off-1">
+                    <ul class="item-list">
+                        <li>Filtros papel</li>
+                        <li>Membranas</li>
+                        <li>Filtros jeringas</li>
+                        <li>Monitores</li>
+                        <li>Filtros cápsula</li>
+                        <li>Centrifugos</li>
+                        <li>Bottle top</li>
+                        <li>Membranas de<br>
+                            transferencia</li>
+                        <li>Mini viales</li>
+                        <li>Mini-arreglos de<br>
+                            proteínas</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="catalog">
+        <div class="wrapper content">
+            <div>content</div>
+        </div>
+    </section>
+    <section class="contact">
+        <div class="wrapper content">
+            <div>content</div>
+        </div>
+    </section>
+</div>
+</body>
+</html>
